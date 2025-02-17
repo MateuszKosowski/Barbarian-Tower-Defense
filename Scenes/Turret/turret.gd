1cbaca27c4732dcc690255fbf4736d44d4f1f44d
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-02-17 22:57:54 +0100

    Tourret is rotating

--- Scenes/Turret/turret.gd
+++ Scenes/Turret/turret.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
 @export var projectile: PackedScene
-
+var enemy_path: Path3D
 @onready var barrels: Array = [$TurretBase/TurretTop/Visor/Barrel, $TurretBase/TurretTop/Visor/Barrel2]
+
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	$".".look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_reload_timeout() -> void:
 	var shot = projectile.instantiate()
@@ -10,6 +14,6 @@ func _on_reload_timeout() -> void:
 	var random_barrel = barrels[randi() % barrels.size()]
 	$".".add_child(shot)
 	shot.global_position = random_barrel.global_position
-	print($".".global_position)
-	print(random_barrel.global_position)
+	shot.direction = $".".global_transform.basis.z
+
 	
--- Scenes/TurretManager.gd
+++ Scenes/TurretManager.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export var turret: PackedScene
+@export var enemy_path: Path3D
 
 func build_turret(turret_pos: Vector3) -> void:
 	var new_turret = turret.instantiate()
 	$".".add_child(new_turret)
 	new_turret.global_position = turret_pos
+	new_turret.enemy_path = enemy_path
 	
