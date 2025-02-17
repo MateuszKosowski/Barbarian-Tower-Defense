301fc785a78d1a762ad7a00b4d9f998511af55b6
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-02-17 21:58:35 +0100

    Add turret

--- Scenes/RayPickerCamera/ray_pick_camera.gd
+++ Scenes/RayPickerCamera/ray_pick_camera.gd
@@ -2,6 +2,7 @@ extends Camera3D
 
 # Export this variable to get access by the inspector
 @export var gridmap: GridMap
+@export var turret_manager: Node3D
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
@@ -11,9 +12,15 @@ func _process(delta: float) -> void:
 	ray_cast_3d.force_raycast_update()
 	
 	if ray_cast_3d.is_colliding():
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			var collision_point = ray_cast_3d.get_collision_point()
-			var cell = gridmap.local_to_map(collision_point)
-			if gridmap.get_cell_item(cell) == 0:
-				gridmap.set_cell_item(cell, 1)
+			if Input.is_action_pressed("click"):
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = gridmap.local_to_map(collision_point)
+				if gridmap.get_cell_item(cell) == 0:
+					gridmap.set_cell_item(cell, 1)
+					var tile_pos = gridmap.map_to_local(cell)
+					turret_manager.build_turret(tile_pos)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
